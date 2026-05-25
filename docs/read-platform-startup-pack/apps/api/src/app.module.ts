ed64c274776856b660381b422521bc63caf72f61
古月 <1724246050@qq.com>
2026-05-25 13:43:18 +0800

    feat: implement chapter retrieval API

--- docs/read-platform-startup-pack/apps/api/src/app.module.ts
+++ docs/read-platform-startup-pack/apps/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './modules/database/database.module';
 import { BookModule } from './modules/book/book.module';
+import { ChapterModule } from './modules/chapter/chapter.module';
 
 @Module({
-  imports: [DatabaseModule, BookModule],
+  imports: [DatabaseModule, BookModule, ChapterModule],
   controllers: [AppController],
   providers: [AppService],
 })
